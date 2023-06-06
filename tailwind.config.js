3095b4887008cac188ba757d59853989671e50b9
Jelena Jovicic <jejovicic@gmail.com>
2023-06-06 14:43:35 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'body': ['Outfit'],
+        'body': ['Outfit', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'fem-soft-blue': 'hsl(215, 51%, 70%)',
+        'fem-cyan': 'hsl(178, 100%, 50%)',
+        'fem-vdk-blue-main': 'hsl(217, 54%, 11%)',
+        'fem-vdk-blue-card': 'hsl(216, 50%, 16%)',
+        'fem-vdk-blue-line': 'hsl(215, 32%, 27%)',
       },
     },
   },
